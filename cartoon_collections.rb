7dddbbbcd13c7d36b09de1d3c677731de57a3a49
John Fuquene <jfuquene@email.wm.edu>
2020-12-27 16:02:27 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,7 +8,7 @@ def greet_characters(array)
 end
 
 def list_dwarves(array)
-  array.each_with_index { |dwarves, index| 
+  list_dwarves(array).each_with_index { |dwarves, index| 
     puts array[dwarves] = index 
   }
   # Use `each_with_index` to enumerate over the provided array
