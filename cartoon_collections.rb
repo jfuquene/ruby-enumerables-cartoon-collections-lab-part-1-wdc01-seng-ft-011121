fb2bff88bad320e381b439781a9ebe6105ef2b4b
John Fuquene <jfuquene@email.wm.edu>
2020-12-27 16:00:02 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,7 +9,7 @@ end
 
 def list_dwarves(array)
   array.each_with_index { |dwarves, index| 
-    p array[dwarves] = index 
+    puts array[dwarves] = index 
   }
   # Use `each_with_index` to enumerate over the provided array
   #
